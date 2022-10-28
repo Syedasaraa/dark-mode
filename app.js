131f92ff213065e166dcbff15386faf341b93e67
syeda sara <66306072+Syedasaraa@users.noreply.github.com>
2022-10-28 19:56:47 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const htmlElements = document.documentElement;
 const checkBox = document.querySelector('input[name="theme"]');
-const paragraph = document.querySelector('p');
-const checked = "And Mine";
+
 
 checkBox.addEventListener("click", function () {
     if (checkBox.checked) {
         htmlElements.classList.toggle('transition');
         htmlElements.setAttribute('data-theme', 'dark');
-        paragraph.innerHTML = checked;
+  
     } else {
         htmlElements.classList.toggle('transition');
         htmlElements.setAttribute('data-theme', 'light');
-        paragraph.innerHTML = "You're an Idiot"
+      
     }
 })
